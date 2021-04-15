07c9f500f03f926071b11c9be1cbeb870025f4ff
Gabriel Domino <domino@unlv.nevada.edu>
2021-04-14 19:16:52 -0700

    Added data retrieval, sometimes times out

--- project.py
+++ project.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
-from nba_api.stats.endpoints import teamgamelogs, leaguegamefinder
+from nba_api.stats.static import teams
+from nba_api.stats.endpoints import leaguegamefinder
 
-# 30000 games since 2014 (includes duplicates)
-leagueGames = leaguegamefinder.LeagueGameFinder(date_from_nullable='10/31/2014').get_data_frames()[0]
+# Get list of all NBA teams
+teams = teams.get_teams()
 
-# 2460 games since 2014 (includes duplicates)
-teamGames = teamgamelogs.TeamGameLogs(date_from_nullable='10/31/2014').get_data_frames()[0]
+# For each NBA team, get all of the games
+leagueGames = pd.DataFrame()
+for team in teams:
+    leagueGames = pd.concat([leagueGames, leaguegamefinder.LeagueGameFinder(team_id_nullable=team['id'], date_from_nullable='10/31/2014',timeout=100).get_data_frames()[0]])
+    print('Done', team['full_name'])
+    
+leagueGames.tocsv('leagueGames.csv')
+    
+# Only keep the necessary columns
+# leagueGames = leagueGames[['GAME_ID','TEAM_ID','WL','FG_PCT','PLUS_MINUS']]
