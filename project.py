6a8cf9c172c92b6aad68f7e7b4240269dc1bf44a
Gabriel Domino <domino@unlv.nevada.edu>
2021-04-14 17:39:26 -0700

    Testing difference between leaguegamefinder and teamgamelogs

--- project.py
+++ project.py
@@ -1,21 +1,9 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 14 15:50:01 2021
-
-@author: Gabriel
-"""
-
 import pandas as pd
-from nba_api.stats.static import teams 
 from nba_api.stats.endpoints import teamgamelogs, leaguegamefinder
 
+# 30000 games since 2014 (includes duplicates)
+leagueGames = leaguegamefinder.LeagueGameFinder(date_from_nullable='10/31/2014').get_data_frames()[0]
 
-teams = teams.get_teams()
-
-team = teams[0]
-# Get all the games for 'team'
-games = teamgamelogs.TeamGameLogs(team_id_nullable=team['id'],date_from_nullable='10/31/2014').get_data_frames()[0]
-games = games[['GAME_ID','GAME_DATE','TEAM_ID','TEAM_NAME','WL','W_RANK','L_RANK','W_PCT_RANK','FG_PCT_RANK','PLUS_MINUS','PLUS_MINUS_RANK']]
-games.to_csv("teamgamelogs.csv")
-
-
+# 2460 games since 2014 (includes duplicates)
+teamGames = teamgamelogs.TeamGameLogs(date_from_nullable='10/31/2014').get_data_frames()[0]
