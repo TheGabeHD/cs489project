fcd0fffd5e4af34746d25478088d9c5028235875
Gabriel Domino <domino@unlv.nevada.edu>
2021-04-16 13:19:03 -0700

    Added league games csv for games since 2014

--- project.py
+++ project.py
@@ -9,7 +9,7 @@ teams = teams.get_teams()
 # For each NBA team, get all of the games
 leagueGames = pd.DataFrame()
 for team in teams:
-    leagueGames = pd.concat([leagueGames, leaguegamefinder.LeagueGameFinder(team_id_nullable=team['id'], date_from_nullable='10/31/2014',timeout=100).get_data_frames()[0]])
+    leagueGames = pd.concat([leagueGames, leaguegamefinder.LeagueGameFinder(team_id_nullable=team['id'], date_from_nullable='10/31/2014', timeout=100).get_data_frames()[0]])
     print('Done', team['full_name'])
     
 leagueGames.tocsv('leagueGames.csv')
