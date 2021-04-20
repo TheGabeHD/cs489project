62d26efb5d1bffb5b81c342318274bd6ea42ac67
Dario <drachen@unlv.nevada.edu>
2021-04-19 17:15:45 -0700

    changed project2 into helperFunctions Class

---
+++ helperFunctions.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from scipy import stats  # stats.zscore(pandaDataFrame)
+
+
+class HelperFunctions:
+    _teamGames = pd.DataFrame()
+    _totalScores = pd.DataFrame()
+
+    def __init__(self):
+        _teamGames = pd.read_csv('TeamGames.csv')
+        _totalScores = pd.read_csv('TotalScores.csv')
+
+    """Grabs the Team ID of the given team"""
+
+    def getTeamId(self, team_name):
+        df = self._teamGames
+        team_id_index = df.loc[df['TEAM_NAME'] == team_name].index[0]
+        return df['TEAM_ID'].loc[team_id_index]
+
+    """Grabs the Team Name of the given team ID"""
+
+    def getTeamName(self, team_id):
+        df = self._teamGames
+        team_name_index = df.loc[df['TEAM_ID'] == team_id].index[0]
+        return df['TEAM_NAME'].loc[team_name_index]
+
+    """Returns an array of Strings containing the Teams playing in the game id given"""
+
+    def getGameTeams(self, game_id):
+        df = self._teamGames
+        team_1_index = df.loc[df['GAME_ID'] == game_id].index[0]
+        team_2_index = df.loc[df['GAME_ID'] == game_id].index[1]
+        return [df['TEAM_NAME'].loc[team_1_index], df['TEAM_NAME'].loc[team_2_index]]
+
+    """Return the Team Name (string) of the winner of game id"""
+
+    def getGameWinner(self, game_id):
+        df = self._teamGames
+        index = df.loc[df['GAME_ID'] == game_id].index[0]
+        teams = self.getGameTeams(game_id)
+        if df['WL'].loc[index] == 'W' and df['TEAM_NAME'].loc[index] == teams[0]:
+            return teams[0]
+        else:
+            return teams[1]
+
+    """Return the Team (string) that was the Home team in game id"""
+
+    def getHomeTeam(self, game_id):
+        df = self._teamGames
+        index = df.loc[df['GAME_ID'] == game_id].index[0]
+        matchup = df['MATCHUP'].loc[index]
+        teams = self.getGameTeams(game_id)
+        if '@' in matchup and df['TEAM_NAME'].loc[index] == teams[0]:
+            return teams[0]
+        else:
+            return teams[1]
+
+    """col_name is string, col num is the index of the column"""
+    def normalizeData(self, col_name, col_num):
+        df = self._teamGames
+        a = df[col_name]
+        n = df.columns[col_num]
+        df[n] = stats.zscore(a)
+        # print(df)
+
+
+"""
+import numpy as np
+from prettytable import PrettyTable
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+
+wl = teamGamesCSV['WL']
+n = wl.shape[0]
+labels = np.zeros(n)
+labels[wl == 'W'] = 1  # ground_truth
+
+cv = KFold(n_splits=10, random_state=225, shuffle=True)
+
+for train_index, test_index in cv.split(teamGamesCSV):
+    X_train, X_test = teamGamesCSV.iloc[train_index, :], teamGamesCSV.iloc[test_index, :]
+    y_train, y_test = labels[train_index], labels[test_index]
+
+"""
