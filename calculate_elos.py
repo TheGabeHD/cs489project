5d5030d2e6bd1a3590043112c1eab802b28f45e3
Levi Yarmak <yarmak@unlv.nevada.edu>
2021-04-19 17:29:38 -0700

    More work

--- calculate_elos.py
+++ calculate_elos.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-import project2 as helper
+import helperFunctions as hf
 
 # Read CSV
 # 'GAME_ID', 'H_TEAM', 'A_TEAM', 'H_POINTS', 'A_POINTS', 'TOTAL', 'M_OF_VIC', 'WINNER'
 gameDataCSV = pd.read_csv('ScoresData.csv')
-additionalgameDataCSV = pd.read_csv('TeamGameSorted.csv')
+additionalgameDataCSV = pd.read_csv('TeamGamesSorted.csv')
 
 # Setting Up Costant Values for ELO Score
 K_VALUE = 20
 HOME_ADVANTAGE = 100
 
-test = helper.getTeamID(gameDataCSV[0]['H_TEAM'])
+test = hf.getTeamId(gameDataCSV[0]['H_TEAM'])
